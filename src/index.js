5af4229b1dc8105ee8a142955452b732767e0ee8
Victor Aldavera <1224100623.vaam@gmail.com>
2026-03-12 21:25:11 -0600

    intento final

--- src/index.js
+++ src/index.js
@@ -10,29 +10,11 @@ import { initBaseDeDatos } from './bd/init.js'
 /**
  * Iniciar el servidor Express después de conectar a la base de datos
  */
-//try {
-  //await initBaseDeDatos()
-  //const PORT = process.env.PORT //|| 3001
-  // app.listen(PORT)
-  //const PORT = process.env.PORT || 3000;
-  //app.listen(PORT, () => {
-  //console.log(`Servidor corriendo en puerto ${PORT}`);
-  //});
-  //console.info(`Servidor Express ejecutandose sobre http://localhost:${PORT}`)
-//} catch (err) {
-  //console.error('Error conecteando a la Base de Datos:', err)
-//}
-
-async function startServer() {
-  try {
-    await initBaseDeDatos();
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-      console.log(`Servidor corriendo en puerto ${PORT}`);
-    });
-  } catch (err) {
-    console.error('Error conectando a la Base de Datos:', err);
-  }
+try {
+  await initBaseDeDatos()
+  const PORT = process.env.PORT //|| 3001
+   app.listen(PORT)
+  console.info(`Servidor Express ejecutandose sobre http://localhost:${PORT}`)
+} catch (err) {
+  console.error('Error conecteando a la Base de Datos:', err)
 }
-
-startServer();
